aa2abddbb559133147cf5d8e9541ab625d83fb52
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2020-04-03 20:00:43 +0100

    Added control panel and cameras

---
+++ Test/Assets/Scripts/Shane/Camera/CCTVScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class CCTVScreen : MonoBehaviour
+{
+    public string CCTVLocation;
+    public int CCTVIndex;
+    public TextMeshPro dateTimeText;
+    public TextMeshPro cameraLocationText;
+
+    private void Start()
+    {
+        cameraLocationText.text = "Cam " + CCTVIndex + " - " + CCTVLocation;
+        StartCoroutine(UpdateDateTime());
+    }
+
+    IEnumerator UpdateDateTime()
+    {
+        while (true)
+        {
+            dateTimeText.text = DateTime.UtcNow.ToString();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
