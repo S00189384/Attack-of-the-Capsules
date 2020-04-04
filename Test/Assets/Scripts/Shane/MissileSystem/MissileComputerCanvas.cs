1cd59977ff123ac3f4b297fb8d2fcab6809bc9d3
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2020-04-05 00:26:55 +0100

    In the process of making a missile system & computer for it

---
+++ Test/Assets/Scripts/Shane/MissileSystem/MissileComputerCanvas.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MissileComputerCanvas : MonoBehaviour
+{
+    public MissileComputerScreen computerScreen;
+
+    [Header("On Screen UI")]
+    public TextMeshProUGUI missileStatusText;
+    public bool FadeMissileStatusText;
+    public float missileFadeSpeed;
+    public Color missileStatusAvailableColour;
+    public Color missileStatusNotAvailableColour;
+    private Color missileStatusCurrentColour;
+
+
+    void Start()
+    {
+        missileStatusText.color = missileStatusAvailableColour;
+        missileStatusCurrentColour = missileStatusText.color;
+
+        StartCoroutine(FadeUIText());
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    //Status Text.
+    public void ResetMissileStatusFade()
+    {
+        FadeMissileStatusText = false;
+        StopCoroutine(FadeUIText());
+    }
+
+    public void SetMissileStatusColour(Color colourToApply)
+    {
+        missileStatusText.color = colourToApply;
+    }
+
+    public IEnumerator FadeUIText()
+    {
+        Color transparentColour = new Color(missileStatusCurrentColour.r, missileStatusCurrentColour.g, missileStatusCurrentColour.b, 0);
+        float fadeTimer = 0;
+
+        while (FadeMissileStatusText)
+        {
+            fadeTimer += Time.deltaTime;
+            missileStatusText.color = Color.Lerp(missileStatusCurrentColour, transparentColour, Mathf.PingPong(fadeTimer * missileFadeSpeed, 1));
+            yield return null;
+        }
+    }
+
+
+}
---
+++ Test/Assets/Scripts/Shane/MissileSystem/MissileComputerScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileComputerScreen : PlayerInteractableObject
+{
+    //Components.
+    public MissileComputerCanvas computerCanvas;
+
+    [Header("Cooldown & Status")]
+    public bool IsInCooldown;
+    public float coolDownCountdownTime = 30f;
+    public float coolDownTimer;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public void ResetCooldownTimer()
+    {
+        coolDownTimer = coolDownCountdownTime;
+    }
+
+    public override void PlayerInteracted()
+    {
+        if(!IsInCooldown)
+        {
+            //Set main message to be green and stop fading.
+            computerCanvas.ResetMissileStatusFade();
+            computerCanvas.SetMissileStatusColour(computerCanvas.missileStatusAvailableColour);
+        }
+    }
+
+    public IEnumerator StartCooldown()
+    {
+        computerCanvas.SetMissileStatusColour(computerCanvas.missileStatusNotAvailableColour);
+        StartCoroutine(computerCanvas.FadeUIText());
+
+        //Activate countdown text on screen;
+
+        while(coolDownTimer >= 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+            yield return null;
+        }
+    }
+}
